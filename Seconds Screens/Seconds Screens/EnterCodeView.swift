7f85452c3c651b770d2b2bf26fb7abffeae72940
analysed-intern-dheerajshyam <analysedinterndheerajshyam@gmail.com>
2024-01-31 22:00:11 +0530

    Modified EnterCodeView source in second screens project for otp fields to have ability to shift focus to adjacent field when pressed next in keyboard, changed font size along with color to white, centered text.

--- Seconds Screens/Seconds Screens/EnterCodeView.swift
+++ Seconds Screens/Seconds Screens/EnterCodeView.swift
@@ -2,8 +2,23 @@ import SwiftUI
 
 struct EnterCodeFields : View {
     
-    @State var email:String = ""
+    @State var otp1:String = ""
+    @State var otp2:String = ""
+    @State var otp3:String = ""
+    @State var otp4:String = ""
+    @State var otp5:String = ""
+    @State var otp6:String = ""
     
+    enum Field: Hashable {
+        case otp1
+        case otp2
+        case otp3
+        case otp4
+        case otp5
+        case otp6
+    }
+    
+    @FocusState private var focusedField:Field?
 
     var body : some View {
         
@@ -12,65 +27,105 @@ struct EnterCodeFields : View {
          */
         
         HStack {
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp1)
                 .frame(width: 50, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
+                }.focused($focusedField, equals: .otp1)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .otp2
                 }
             
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp2)
                 .frame(width: 50, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
+                }.focused($focusedField, equals: .otp2)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .otp3
                 }
             
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp3)
                 .frame(width: 50, height: 50)
+                
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
+                }.focused($focusedField, equals: .otp3)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .otp4
                 }
             
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp4)
                 .frame(width: 50, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
+                }.focused($focusedField, equals: .otp4)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .otp5
                 }
             
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp5)
                 .frame(width: 50, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
+                }.focused($focusedField, equals: .otp5)
+                .submitLabel(.next)
+                .onSubmit {
+                    focusedField = .otp6
                 }
             
-            TextField("", text: self.$email)
+            TextField("", text: self.$otp6)
                 .frame(width: 50, height: 50)
+                .foregroundStyle(.white)
+                .font(.system(size: 30))
+                .multilineTextAlignment(.center)
                 .background {
                     RoundedRectangle(cornerRadius: 15)
                         .fill(
                             Color(red: 0.63, green: 0.63, blue: 0.63).opacity(0.29)
                         ).frame(width: 50, height: 50)
                         
-                }
+                }.focused($focusedField, equals: .otp6)
+            
         }.padding(.top, 8)
     }
 }
